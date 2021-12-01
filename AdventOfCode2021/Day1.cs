a5dee23a53e6942d27317fc3b223ef7b5732bbcb
Sonkerud <alexandersonerud@hotmail.com>
2021-12-01 13:30:06 +0100

    hgf

--- AdventOfCode2021/Day1.cs
+++ AdventOfCode2021/Day1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AdventOfCode2021
 {
     public class Day1
@@ -15,6 +16,7 @@ namespace AdventOfCode2021
             var todaysInputData = AdventOfCode2021.Inputreader.ReadTxt("Day1");
 
             Console.WriteLine(todaysInputData.Where((x, i) => i > 0 && x > todaysInputData[i - 1]).ToList().Count.ToString());
+            Console.WriteLine(todaysInputData.Where((x, i) => i > 2 && x > todaysInputData[i - 3]).ToList().Count.ToString());
 
         }
 
@@ -22,20 +24,23 @@ namespace AdventOfCode2021
         {
             var todaysInputData = AdventOfCode2021.Inputreader.ReadTxt("Day1");
 
-          
-            // Console.WriteLine(nrSeries.Where((x, i) => i > 0 && x > nrSeries[i - 1]).ToList().Count.ToString());
-            //Console.WriteLine(todaysInputData.Select((x,i) => i<todaysInputData.Count -2 ? x = x + todaysInputData[i+1] + todaysInputData[i+2]:x=x).ToList().Where((x, i) => i > 0 && x > todaysInputData[i - 1]).ToList().Count.ToString());
-            /*var list1 = todaysInputData.Select((x, i) => 
-            i < todaysInputData.Count - 3 
-            && x + todaysInputData[i + 1] + todaysInputData[i + 2] < todaysInputData[i + 1] + todaysInputData[i + 2] + todaysInputData[i + 3]
-            ? x = 1 : 1+1).ToList().Sum().ToString();*/
-            
-            Console.WriteLine(todaysInputData.Select((x, i) =>
+            /* Console.WriteLine(todaysInputData.Select((x, i) =>
             i < todaysInputData.Count - 3 &&
             x + todaysInputData[i + 1] + todaysInputData[i + 2] < todaysInputData[i + 1] + todaysInputData[i + 2] + todaysInputData[i + 3] ?
-            x = 1 : 0 + 0).ToList().Sum().ToString());
-                }
+            x = 1 : 0 + 0).ToList().Sum().ToString());*/
 
+            Console.WriteLine(todaysInputData.Select((x, i) =>
+            i< todaysInputData.Count - 3 &&
+            todaysInputData.GetRange(i,3).Sum() < todaysInputData.GetRange(i+1, 3).Sum() ?
+            x = 1 : 0).ToList().Sum().ToString());
+            
+            
+            var test = todaysInputData.Where((e, i) => i >= 3 && e > todaysInputData[i - 3]).ToList();
 
+            Console.WriteLine(todaysInputData.Where((e, i) => i >= 3 && e > todaysInputData[i - 3])
+            .Count());
+             
+
+        }
     }
 }
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-AdventOfCode2021.Day1.Day1Part2Calculator();
+AdventOfCode2021.Day1.Day1Part1Calculator();
 
