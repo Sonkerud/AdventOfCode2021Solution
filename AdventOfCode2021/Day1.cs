80b0392c44c508a47dffd3d496fc0a6f0d2f2c8f
Sonkerud <alexandersonerud@hotmail.com>
2021-12-03 12:02:26 +0100

    Day3

--- AdventOfCode2021/Day1.cs
+++ AdventOfCode2021/Day1.cs
@@ -14,10 +14,7 @@ namespace AdventOfCode2021
         public static void Day1Part1Calculator()
         {
             var todaysInputData = AdventOfCode2021.Inputreader.ReadTxtInt("Day1","Day1");
-
             Console.WriteLine(todaysInputData.Where((x, i) => i > 0 && x > todaysInputData[i - 1]).ToList().Count.ToString());
-            Console.WriteLine(todaysInputData.Where((x, i) => i > 2 && x > todaysInputData[i - 3]).ToList().Count.ToString());
-
         }
 
 
@@ -27,25 +24,10 @@ namespace AdventOfCode2021
         {
             var todaysInputData = AdventOfCode2021.Inputreader.ReadTxtInt("Day1","Day1");
 
-            /* Console.WriteLine(todaysInputData.Select((x, i) =>
-            i < todaysInputData.Count - 3 &&
-            x + todaysInputData[i + 1] + todaysInputData[i + 2] < todaysInputData[i + 1] + todaysInputData[i + 2] + todaysInputData[i + 3] ?
-            x = 1 : 0 + 0).ToList().Sum().ToString());*/
-
             Console.WriteLine(todaysInputData.Select((x, i) =>
             i< todaysInputData.Count - 3 &&
             todaysInputData.GetRange(i,3).Sum() < todaysInputData.GetRange(i+1, 3).Sum() ?
             x = 1 : 0).ToList().Sum().ToString());
-            
-            
-            var test = todaysInputData.Where((e, i) => i >= 3 && e > todaysInputData[i - 3]).ToList();
-
-            Console.WriteLine(todaysInputData.Where((e, i) => i >= 3 && e > todaysInputData[i - 3])
-            .Count());
-             
-
         }
-
-
     }
 }
---
+++ AdventOfCode2021/Day3.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    public static class Day3
+    {
+        public static void Day3Part1Calculator()
+        {
+            var todaysInputData = Inputreader.ReadTxtString("Day3", "Day3");
+            
+            string gamma = "";
+            string epsilon = "";
+
+            for (int i = 0; i < todaysInputData[0].Length; i++)
+            {
+                if (todaysInputData.Select(x => int.Parse(x[i].ToString())).ToList().Sum() > todaysInputData.Count / 2)
+                {
+                    gamma += "1";
+                    epsilon += "0";
+                }
+                else
+                {
+                    gamma += "0";
+                    epsilon += "1";
+                }
+            }
+            int gammaInt = Convert.ToInt32(gamma, 2);
+            int epsilonInt = Convert.ToInt32(epsilon, 2);
+            Console.WriteLine(gammaInt + " * " + epsilonInt + " = " + gammaInt * epsilonInt);
+
+        }
+
+        public static void Day3Part2Calculator()
+        {
+            var todaysInputData = Inputreader.ReadTxtString("Day3", "Day3");
+            List<string> filteredListOGR = todaysInputData;
+            List<string> filteredListCSR = todaysInputData;
+            int ogr = 0;
+            int csr = 0;
+
+            for (int i = 0; i < todaysInputData[0].Length; i++)
+            {   
+                if (filteredListOGR.Count > 1)
+                {
+                    filteredListOGR = FilterList(filteredListOGR, i, "ogr");
+                    if(filteredListOGR.Count == 1)
+                    {
+                        ogr = Convert.ToInt32(filteredListOGR[0], 2);
+                        
+                    }
+                }
+
+                if (filteredListCSR.Count > 1)
+                {
+                    filteredListCSR = FilterList(filteredListCSR, i, "csr");
+                    if (filteredListCSR.Count == 1)
+                    {
+                        csr = Convert.ToInt32(filteredListCSR[0], 2); 
+                    }
+                }
+            }
+            Console.WriteLine("ogr: " + ogr + " * csr: " + csr + " = " + ogr * csr);
+        }
+
+        public static List<string> FilterList(List<string> todaysInputData, int i, string ogrOrcsr)
+        {
+           
+            int nrOf1 = todaysInputData.Where(x => x[i].Equals('1')).ToList().Count;
+            int nrOf0 = todaysInputData.Where(x => x[i].Equals('0')).ToList().Count;
+            int nrOfBits = todaysInputData.Count;
+
+            if (nrOf1 > nrOf0) // Fler 1:or än 0:or
+            {
+                switch (ogrOrcsr)
+                {
+                    case "ogr": return todaysInputData.Where(x => x[i].Equals('1')).ToList();
+                    default: return todaysInputData.Where(x => x[i].Equals('0')).ToList();
+                }
+            }
+            else if (nrOf1 < nrOf0)
+            {
+
+                switch (ogrOrcsr)
+                {
+                    case "ogr": return todaysInputData.Where(x => x[i].Equals('0')).ToList();
+                    default: return todaysInputData.Where(x => x[i].Equals('1')).ToList();
+                }
+            }
+            else 
+            {
+                if (ogrOrcsr == "ogr")
+                {
+                    return todaysInputData.Where(x => x[i].Equals('1')).ToList();
+                }
+                else
+                {
+                    return todaysInputData.Where(x => x[i].Equals('0')).ToList();
+                }
+            }
+        }
+
+
+
+
+    }
+}
