16a6156ec6edd01267ad1b914aec55dc31bec6e3
Sonkerud <alexandersonerud@hotmail.com>
2021-12-11 20:27:29 +0100

    Day10

--- AdventOfCode2021/Day10.cs
+++ AdventOfCode2021/Day10.cs
@@ -12,156 +12,75 @@ namespace AdventOfCode2021
         public static void Day10Part1Calculator()
         {
             var todaysInputData = AdventOfCode2021.Inputreader.ReadTxtString("Day10", "Day10");
-            List<int> results = new List<int>();    
+            List<int> results = new List<int>();
+            List<long> resultsPart2 = new List<long>();
+            List<string> completion = new List<string>();
 
             for (int i = 0; i < todaysInputData.Count(); i++)
             {
                 var line = todaysInputData[i];
-            
+                var tempLine = todaysInputData[i];
                 List<string> openChunks = new List<string>();
 
-                line = line.Replace("<>", "");
-                line = line.Replace("()", "");
-                line = line.Replace("{}", "");
-                line = line.Replace("[]", "");
-                line = line.Replace("<>", "");
-                line = line.Replace("()", "");
-                line = line.Replace("{}", "");
-                line = line.Replace("[]", "");
-                line = line.Replace("<>", "");
-                line = line.Replace("()", "");
-                line = line.Replace("{}", "");
-                line = line.Replace("[]", "");
-                line = line.Replace("<>", "");
-                line = line.Replace("()", "");
-                line = line.Replace("{}", "");
-                line = line.Replace("[]", "");
-                line = line.Replace("<>", "");
-                line = line.Replace("()", "");
-                line = line.Replace("{}", "");
-                line = line.Replace("[]", "");
-                line = line.Replace("<>", "");
-                line = line.Replace("()", "");
-                line = line.Replace("{}", "");
-                line = line.Replace("[]", "");
-
+                while (line.Contains("<>") || line.Contains("{}") || line.Contains("()") || line.Contains("[]"))
+                {
+                    line = line.Replace("<>", "");
+                    line = line.Replace("()", "");
+                    line = line.Replace("{}", "");
+                    line = line.Replace("[]", "");
+                }
 
                 if (line.Contains(">") || line.Contains("}") || line.Contains(")") || line.Contains("]"))
                 {
-                    Console.WriteLine(line);
-                    string expected = "";
-                    //line.ElementAt(line.First(x => x == '>' || x == '}' || x == ')' || x == ']').);
                     var expectedTurnIt = line.ElementAt(line.IndexOf(line.First(x => x == '>' || x == '}' || x == ')' || x == ']')));
                     if (expectedTurnIt == ']')
                     {
-                       expected = "]";
                         results.Add(57);
                     }
                     if (expectedTurnIt == ')')
                     {
-                        expected = ")";
                         results.Add(3);
                     }
                     if (expectedTurnIt == '>')
                     {
-                        expected = ">";
                         results.Add(25137);
                     }
                     if (expectedTurnIt == '}')
                     {
-                        expected = "}";
                         results.Add(1197);
                     }
-
-                    Console.WriteLine(expected);
-
                 }
-               
+                else
+                {
+                    completion = line.Reverse().ToList().Select(x => x.ToString()).ToList();
+                    long score = 0;
+
+                    for (int l = 0; l < completion.Count(); l++)
+                    {
+                        score = score * 5;
+                        switch (completion[l])
+                        {
+                            case "(": score += 1; break;
+                            case "[": score += 2; break;
+                            case "{": score += 3; break;
+                            case "<": score += 4; break;
+                        }
+                    }
+                    resultsPart2.Add(score);
+                }
             }
+            //Part 2
+            var part2 = resultsPart2.OrderBy(x => x).ToList();
+            Console.WriteLine(resultsPart2.OrderBy(x => x).ElementAt((resultsPart2.Count() / 2)));
+
+            // Part 1
             var resultsGrouped = results.GroupBy(x => x).ToList();
             int output = 0;
             for (int p = 0; p < resultsGrouped.Count; p++)
             {
                 output += resultsGrouped[p].Count() * resultsGrouped[p].Key;
             }
-
             Console.WriteLine(output);
         }
     }
 }
-//Console.WriteLine("heh");
-//Console.WriteLine($"Nr of ( = {line.Where(x => x.Equals('(')).ToList().Count()}");
-//Console.WriteLine($"Nr of ) = {line.Where(x => x.Equals(')')).ToList().Count()}");
-//Console.WriteLine($"Nr of x = {line.Where(x => x.Equals('{')).ToList().Count()}");
-//Console.WriteLine($"Nr of x = {line.Where(x => x.Equals('}')).ToList().Count()}");
-//Console.WriteLine($"Nr of [ = {line.Where(x => x.Equals('[')).ToList().Count()}");
-//Console.WriteLine($"Nr of ] = {line.Where(x => x.Equals(']')).ToList().Count()}");
-//Console.WriteLine($"Nr of < = {line.Where(x => x.Equals('<')).ToList().Count()}");
-//Console.WriteLine($"Nr of > = {line.Where(x => x.Equals('>')).ToList().Count()}");
-
-/* for (int y = 0; y < line.Length; y++)
- {
-
-     var c = line[i];
-
-     switch (c.ToString())
-     {
-         case "{":
-             openChunks.Add(c.ToString());
-             break;
-         case "(":
-             openChunks.Add(c.ToString());
-             break;
-         case "<":
-             openChunks.Add(c.ToString());
-             break;
-         case "[":
-             openChunks.Add(c.ToString());
-             break;
-
-         case "}":
-             {
-                 if (openChunks.Any(x => x == "{"))
-                 {
-                     openChunks.RemoveAt(openChunks.LastIndexOf("{"));
-                 }
-                 else
-                 {
-                     Console.WriteLine(c.ToString());
-                 }
-             }
-             break;
-         case ")":
-             if (openChunks.Any(x => x == "("))
-             {
-                 openChunks.RemoveAt(openChunks.LastIndexOf("("));
-             }
-             else
-             {
-                 Console.WriteLine(c.ToString());
-             }
-             break;
-         case ">":
-             if (openChunks.Any(x => x == "<"))
-             {
-                 openChunks.RemoveAt(openChunks.LastIndexOf("<"));
-             }
-             else
-             {
-                 Console.WriteLine(c.ToString());
-             }
-             break;
-         case "]":
-             if (openChunks.Any(x => x == "["))
-             {
-                 openChunks.RemoveAt(openChunks.LastIndexOf("["));
-             }
-             else
-             {
-                 Console.WriteLine(c.ToString());
-             }
-             break;
-
-     }
- }
-*/
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 AdventOfCode2021.Day10.Day10Part1Calculator();
+//AdventOfCode2021.DayTen dayTen = new AdventOfCode2021.DayTen();
+
+//dayTen.Process();
+
 
 
 
